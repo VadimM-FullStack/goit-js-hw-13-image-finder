320901927414c30eb20d8418233b76b90e793ab4
Vadim Matsko <mvadim3101@gmail.com>
2021-03-14 19:33:39 +0200

    First Step

--- src/index.js
+++ src/index.js
@@ -1,1 +1,50 @@
-import './styles.css';
+import photoTemplate from "./templates/photo-card.hbs";
+import PhotosApiService from "./js/apiService.js";
+import LoadMoreBtn from "./js/load-more-btn.js";
+import "./styles.css";
+
+const refs = {
+  searchForm: document.querySelector(".js-search-form"),
+  photosContainer: document.querySelector(".js-photos-container"),
+};
+
+const loadMoreBtn = new LoadMoreBtn({
+  selector: '[data-action="load-more"]',
+  hidden: true,
+});
+
+const photosApiService = new PhotosApiService();
+
+refs.searchForm.addEventListener("submit", onSearch);
+loadMoreBtn.refs.button.addEventListener("click", fetchPhotos);
+
+function onSearch(e) {
+  e.preventDefault();
+
+  photosApiService.query = e.currentTarget.elements.query.value;
+
+  if (photosApiService.query === "") {
+    return alert("Make the correct input");
+  }
+
+  loadMoreBtn.show();
+  photosApiService.resetPage();
+  clearPhotosContainer();
+  fetchPhotos();
+}
+
+function fetchPhotos() {
+  loadMoreBtn.disable();
+  photosApiService.fetchPhotos().then(photos => {
+    appendPhotosMarkup(photos);
+    loadMoreBtn.enable();
+  });
+}
+
+function appendPhotosMarkup(photos) {
+  refs.photosContainer.insertAdjacentHTML("beforeend", photoTemplate(photos));
+}
+
+function clearPhotosContainer() {
+  refs.photosContainer.innerHTML = "";
+}
---
+++ src/js/apiService.js
@@ -0,0 +1,36 @@
+const BASE_URL = "https://pixabay.com/api/";
+const PIXABAY_KEY = "20677562-9c517eaf9134a4d9aa45dfdde";
+
+export default class PhotosApiService {
+  constructor() {
+    this.searchQuery = "";
+    this.page = 1;
+  }
+
+  fetchPhotos() {
+    const url = `${BASE_URL}?key=${PIXABAY_KEY}&q=${this.searchQuery}&image_type=photo&orientation=horizontal&page=${this.page}&per_page=12`;
+
+    return fetch(url)
+      .then(response => response.json())
+      .then(({ photos }) => {
+        this.incrementPage();
+        return photos;
+      });
+  }
+
+  incrementPage() {
+    this.page += 1;
+  }
+
+  resetPage() {
+    this.page = 1;
+  }
+
+  get query() {
+    return this.searchQuery;
+  }
+
+  set query(newQuery) {
+    this.searchQuery = newQuery;
+  }
+}
